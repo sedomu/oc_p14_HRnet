40689e40805eece5235ce1e67ab2a83ff8b32f68
sedomu <seguin@lilo.org>
2025-08-13 19:46:31 +0200

    Beginning react-table implementation

--- hrnet/src/pages/EmployeeList.jsx
+++ hrnet/src/pages/EmployeeList.jsx
@@ -1,15 +1,106 @@
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useMemo, useState } from 'react'
+import {
+    flexRender,
+    getCoreRowModel, getSortedRowModel,
+    useReactTable
+} from '@tanstack/react-table'
 
 export default function EmployeeList() {
     const employees = useSelector(state => state.employees)
     console.log(employees)
+
+    const[sorting, setSorting] = useState([])
+
+    const columns = useMemo(() => [
+        {
+            header: 'First Name',
+            accessorKey: 'firstName',
+            enableSorting: true,
+        },
+        {
+            header: 'Last Name',
+            accessorKey: 'lastName',
+            enableSorting: true,
+        }
+    ], [])
+
+    const table = useReactTable({
+        data: employees,
+        columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+    })
 
     return <>
         <div id="employee-div" className="container">
             <h1>Current Employees</h1>
             <table id="employee-table" className="display"></table>
             <Link to="/">Home</Link>
+
+            <table>
+                <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => {
+                            return (
+                                <th
+                                    key={header.id}
+                                    onClick={header.column.getToggleSortingHandler()}
+                                    style={{ cursor: 'pointer' }}
+                                >
+                                    {header.isPlaceholder ? null : (
+                                        <>
+                                            {flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext()
+                                            )}
+                                            {{
+                                                asc: ' 🔼',
+                                                desc: ' 🔽',
+                                            }[header.column.getIsSorted()] ?? null}
+                                        </>
+                                    )}
+                                </th>
+                            )
+                        })}
+                    </tr>
+                ))}
+                </thead>
+                <tbody>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id}>
+                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                {table.getFooterGroups().map(footerGroup => (
+                    <tr key={footerGroup.id}>
+                        {footerGroup.headers.map(header => (
+                            <th key={header.id}>
+                                {header.isPlaceholder
+                                    ? null
+                                    : flexRender(
+                                        header.column.columnDef.footer,
+                                        header.getContext()
+                                    )}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </tfoot>
+            </table>
+
         </div>
     </>
 }
