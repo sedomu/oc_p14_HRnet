4f62d982aa81c51eff3c6dc7462a5cd86f529ecc
sedomu <seguin@lilo.org>
2025-08-19 12:54:52 +0200

    testing with my dialog package published onto npm

--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import {Form} from "@/components/Form.tsx";
 import {useState} from "react";
-import Dialog from "@/components/Dialog/Dialog.tsx";
+// import {TinyReactDialog} from "tiny-react-dialog";
+// import { TinyReactDialog } from "tiny-react-dialog";
+// import Dialog from "@/components/Dialog/Dialog";
+// import {TinyReactDialog} from "tiny-react-dialog";
+import {TinyReactDialog} from "tiny-react-dialog";
 
 export default function Home() {
     const [isDialogVisible, setIsDialogVisible] = useState(false)
@@ -8,9 +12,15 @@ export default function Home() {
         <div className="flex w-full max-w-sm flex-col gap-6">
             <Form onSuccess={() => setIsDialogVisible(true)}/>
 
-            <Dialog visible={isDialogVisible} onClose={() => {setIsDialogVisible(false)}}>
-                Employee Created!
-            </Dialog>
+            {/*<Dialog visible={isDialogVisible} onClose={() => {setIsDialogVisible(false)}}>*/}
+            {/*    Employee Created!*/}
+            {/*</Dialog>*/}
+            {/*<TinyReactDialog visible={isDialogVisible} onClose={() => {setIsDialogVisible(false)}}>*/}
+            {/*    C'est mon tiny*/}
+            {/*</TinyReactDialog>*/}
+            <TinyReactDialog visible={isDialogVisible} onClose={() => {setIsDialogVisible(false)}}>
+                <div>C'est mon tiny from npm !!!</div>
+            </TinyReactDialog>
         </div>
     </>
 }
