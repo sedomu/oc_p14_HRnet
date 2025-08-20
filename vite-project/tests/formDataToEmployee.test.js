bc75ef15d6a3ed0f380ce355ce99cc8886dc3596
sedomu <seguin@lilo.org>
2025-08-20 13:33:30 +0200

    implementing vitest for unit tests

---
+++ vite-project/tests/formDataToEmployee.test.js
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+import formDataToEmployee from "@/components/form/formDataToEmployee.js";
+
+describe("formDataToEmployee", () => {
+    it("should return employee object", () => {
+        let formData = new FormData();
+        formData.append("firstName", "John");
+        formData.append("lastName", "Doe");
+        formData.append("dateOfBirth", "2000-01-01");
+        formData.append("startDate", "2022-01-01");
+        formData.append("street", "123 Main street");
+        formData.append("city", "Anytown");
+        formData.append("state", "NY");
+        formData.append("zipCode", "12345");
+        formData.append("department", "Sales");
+
+        const employee = formDataToEmployee(formData);
+
+        expect(employee.firstName).toBe("John");
+        expect(employee.lastName).toBe("Doe");
+        expect(employee.dateOfBirth).toBe("2000-01-01");
+        expect(employee.startDate).toBe("2022-01-01");
+        expect(employee.street).toBe("123 Main street");
+        expect(employee.city).toBe("Anytown");
+        expect(employee.state).toBe("NY");
+        expect(employee.zipCode).toBe(12345);
+        expect(employee.department).toBe("Sales");
+    });
+});
